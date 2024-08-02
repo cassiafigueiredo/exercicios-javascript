b925a2fa50c9f39673d839434b0054b5a0438aa1
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2024-08-01 23:42:09 -0300

    codigo final

--- exOlaUser/script.js
+++ exOlaUser/script.js
@@ -1,6 +1,8 @@
-
-let nome = document.getElementById('inputNome');
-
-GerarMensagem() {
-document.write(<p>olá ${nome}! </p>);
+function gerarMensagem() {
+    const input = document.getElementById('inputNome')
+    const nome = input.value;
+    const mensagem = document.getElementById('mensagemNome')
+    
+    mensagem.textContent = `Olá ${nome}! Seja muito Bem-Vindo(a)!`
 }
+
