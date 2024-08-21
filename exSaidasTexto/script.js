8b6a852d28a4ebfc0c0346cfb913cd0d9d162132
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2024-08-21 15:37:07 -0300

    alguns exercicios

---
+++ exSaidasTexto/script.js
@@ -0,0 +1,9 @@
+//var pote = "bombom"
+//document.getElementById('txtDinamico').innerHTML="este é um texto <b>dinâmico</b>"
+//alert(pote)
+
+
+
+let nome = 'cassia figueiredo';
+nome = 'patrik silveira'
+console.log(nome)
